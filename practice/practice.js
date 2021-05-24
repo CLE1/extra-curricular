811cacbb4d88ae177eabc4b37545ca2935526dd9
Christopher <espinoza.christopher117@gmail.com>
2021-05-24 08:37:53 -0500

    break

--- practice/practice.js
+++ practice/practice.js
@@ -5,3 +5,14 @@
 var carName = "Volvo"
 
 
+// Create a variable called x, assign the value 50 to it.
+
+var x = 50
+
+// Display the sum of 5 + 10, using two variables: x and y.
+
+var x = 5
+var y = 10
+
+document.getElementById("demo").innerHTML = x + y;
+
