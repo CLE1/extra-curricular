2214b8ed4a3a7e3e79c7d75b49bc2be43cae880d
Christopher <espinoza.christopher117@gmail.com>
2021-10-07 23:41:28 -0500

    break

--- java practice/src/practice.java
+++ java practice/src/practice.java
@@ -10,6 +10,35 @@ public class practice {
         return false;
     }
 
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if (aSmile && bSmile) {
+            return true;
+        }
+        if (!aSmile && !bSmile) {
+            return true;
+        }
+        return false;
+
+        public int sumDouble ( int a, int b){
+            // Store the sum in a local variable
+            int sum = a + b;
+
+            // Double it if a and b are the same
+            if (a == b) {
+                sum = sum * 2;
+            }
+
+            return sum;
+        }
 
 
+    }
+
+    public int diff21(int n) {
+        if (n <= 21) {
+            return 21 - n;
+        } else {
+            return (n - 21) * 2;
+        }
+    }
 }
