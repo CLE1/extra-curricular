5df4ccb6a1b4d5f30055771f7834da3e8c0a4d64
Christopher <espinoza.christopher117@gmail.com>
2021-10-11 23:13:11 -0500

    break

--- java practice/src/practice.java
+++ java practice/src/practice.java
@@ -50,4 +50,9 @@ public class practice {
     public boolean makes10(int a, int b) {
         return (a == 10 || b == 10 || a+b == 10);
     }
+
+    public boolean nearHundred(int n) {
+        return ((Math.abs(100 - n) <= 10) ||
+                (Math.abs(200 - n) <= 10));
+    }
 }
